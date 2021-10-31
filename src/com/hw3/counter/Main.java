ac27d8b52e6c18bfc60c426cd891b5e707068cc3
VitaliyR <vitaliy.rakov@gmail.com>
2021-10-31 16:02:58 +0300

    hw3

---
+++ src/com/hw3/counter/Main.java
@@ -0,0 +1,30 @@
+package com.hw3.counter;
+
+public class Main {
+    private static final int CNT = 5000;
+
+    public static void main(String[] args) {
+
+        Counter counter = new Counter(0);
+        System.out.println("Начальное состояние счетчика: " + counter.get());
+
+        Thread increment = new Thread(() -> {
+            for (int i = 0; i < CNT; i++) {
+                counter.increment();
+
+            }
+        });
+
+        Thread decrement = new Thread(() -> {
+            for (int i = 0; i < CNT; i++) {
+                counter.decrement();
+            }
+        });
+
+        increment.start();
+        decrement.start();
+
+        System.out.println("Конечное состояние счетчика: " + counter.get());
+
+    }
+}
